835a1d204246322492f6a24fe40a11930d45388d
Admin_RALD <admin@rald.cl>
2024-06-07 14:59:10 -0400

    password-recovery fix

--- application/views/login/password_recovery.php
+++ application/views/login/password_recovery.php
@@ -1,71 +1,80 @@
 <?php echo $header; ?>
-    <section  class="slide inner">
-      <div class="container inner-top">
-        <?php if($this->session->flashdata('msg')){ ?>
-        <div class="row">
-          <div class="col-md-12"><?php echo $this->session->flashdata('msg'); ?>          </div>
-        </div>
-      <?php } ?>
-      <div class="row ">
-        <div class="col-md-6 content-detail">
-                  
-          <h1>Recupera tu password</h1>
-          <p>
-            Ingresa el email registrado para enviarte una nueva clave:
-          </p>
+<section class="slide inner">
+  <div class="container inner-top">
+    <?php if ($this->session->flashdata('msg')) { ?>
+      <div class="row">
+        <div class="col-md-12"><?php echo $this->session->flashdata('msg'); ?> </div>
+      </div>
+    <?php } ?>
+    <div class="row ">
+      <div class="col-md-6 content-detail">
 
-            <form action="<?php echo base_url('clogin/getPassword')?>" method="post" role="form" data-toggle="validator">
-                <div class="form-group input-group-lg">
-                  <label>Email address</label>
-                  <input type="email" name="emailUser" class="form-control" aria-describedby="emailHelp" placeholder="E-mail" required>
-                  <div class="help-block with-errors"></div>
-                </div>
-                  <!-- <input type="hidden" name="backto" class="form-control" value="<?php echo $backto?>"> -->
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>
-        </div>
+        <h1>Recupera tu password</h1>
+        <p>
+          Ingresa el email registrado para enviarte una nueva clave:
+        </p>
 
-        <div class="col-md-6 content-detail">
-          <img src="<?php echo base_url("/public/imgs/1mp.svg") ?>"/>
-          
-          
-        </div>
+        <form action="<?php echo base_url('clogin/getPassword') ?>" method="post" role="form" data-toggle="validator" id="form" novalidate>
+          <div class="form-group input-group-lg">
+            <label>Email address</label>
+            <input type="email" name="emailUser" class="form-control" aria-describedby="emailHelp" placeholder="E-mail" required>
+            <div class="help-block with-errors"></div>
+          </div>
+          <!-- <input type="hidden" name="backto" class="form-control" value="<?php echo $backto ?>"> -->
+          <button type="submit" class="button-primary">Enviar</button>
+        </form>
+      </div>
 
+      <div class="col-md-6 content-detail">
+        <img src="<?php echo base_url("/public/imgs/1mp.svg") ?>" />
 
 
       </div>
 
+
+
     </div>
 
-    
-  </section>
+  </div>
 
- <script type="text/javascript">
-      
-        $( "#email" ).blur(function() {
 
-         var email = $("#email").val();
+</section>
 
-          if(email){
-            $.ajax({
-            url: "clogin/validaUser",
-            type: "POST",
-            data: { email: email},
-            context: document.body
-              }).done(function(data) {
-            
-              if(data==false){
-                 $('#submit').attr("disabled", false);
-                 $('#alert').removeClass('ver');
-              }else{
-                 $('#submit').attr("disabled", true);
-                 $('#alert').addClass('ver');
-            }
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#form').on('submit', function(event) {
+      var form = this;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    });
+    $("#email").blur(function() {
 
-          });
-        }
+      var email = $("#email").val();
+
+      if (email) {
+        $.ajax({
+          url: "clogin/validaUser",
+          type: "POST",
+          data: {
+            email: email
+          },
+          context: document.body
+        }).done(function(data) {
+
+          if (data == false) {
+            $('#submit').attr("disabled", false);
+            $('#alert').removeClass('ver');
+          } else {
+            $('#submit').attr("disabled", true);
+            $('#alert').addClass('ver');
+          }
+
         });
-
-
-    </script>
+      }
+    });
+  });
+</script>
 <?php echo $footer; ?>
